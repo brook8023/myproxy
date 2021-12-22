da8e3d5e9e2245591b7e54da654ecdc115be40cd
aupokh <91663540+aupokh@users.noreply.github.com>
2021-12-22 22:31:39 +0800

    Update setup_c3pool_miner.sh
    update mine.c3pool.com

--- setup_c3pool_miner.sh
+++ setup_c3pool_miner.sh
@@ -248,7 +248,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "auto.c3pool.org:'$PORT'",/' $HOME/c3pool/config.json
+sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:'$PORT'",/' $HOME/c3pool/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/c3pool/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/c3pool/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/c3pool/config.json
