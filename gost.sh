4984bcb41189bb2c41ba74d8aefcb572e829378e
brook8023 <brook8023@gmail.com>
2024-01-21 13:15:27 +0800

    first

--- gost.sh
+++ gost.sh
@@ -4,7 +4,3 @@ gzip -d gost-linux-amd64-2.11.2.gz
 chmod +x gost-linux-amd64-2.11.2
 nohup ./gost-linux-amd64-2.11.2 -L=socks5://brook:brook@:8023 &
 
-# add docker
-apt update -y
-curl -s -L https://get.docker.com | LC_ALL=en_US.UTF-8 bash
-docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token Jvxw6A/pc2GhdM/JtHpRqncfnOuMbxXQGLlx+MZQj4Q=
